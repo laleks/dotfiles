64217451465e66e7bafd3755e99ae1efd72fb6e5
laleks <54145@mail.ru>
2024-02-14 16:21:11 +0500

    somech

--- fish/config.fish
+++ fish/config.fish
@@ -9,11 +9,12 @@ if status is-login
 end
 
 alias sudo="sudo -E"
-alias ks="/home/lt/.local/share/applications/kaspro_start.sh"
-alias kc="/home/lt/.local/share/applications/kaspro_coding.sh"
-alias ltbackup="sudo /home/lt/.local/share/ltscript/ltbackup.sh"
+#alias ks="/home/lt/.local/share/applications/kaspro_start.sh"
+#alias kc="/home/lt/.local/share/applications/kaspro_coding.sh"
 alias sotkaftp="curlftpfs ftp://user:say22say@192.168.0.61:2221 /home/lt/ftp/"
 alias usotkaftp="fusermount -u /home/lt/ftp"
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
+alias backup-make='/mnt/data/lt/dotfiles/script/backup_make.sh'
+
 
