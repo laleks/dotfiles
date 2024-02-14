e53a61f4ca93f4f06d531c46f64ead087a042260
laleks <54145@mail.ru>
2024-02-14 16:03:54 +0500

    some change

---
+++ script/backup_make.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p 'Next sync with /mnt/multi' var3
+rsync -vrt --progress --delete /mnt/data/distrib /mnt/multi/backup/
+rsync -vrt --progress --delete /mnt/data/homem /mnt/multi/backup/
+rsync -vrt --progress --delete /mnt/data/lt /mnt/multi/backup/
+
+
+read -p 'Next sync with /mnt/flash' var3
+rsync -vrt --progress --delete /mnt/data/distrib /mnt/flash/backup/
+rsync -vrt --progress --delete /mnt/data/homem /mnt/flash/backup/
+rsync -vrt --progress --delete /mnt/data/lt /mnt/flash/backup/
